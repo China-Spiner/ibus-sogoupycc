c0814909a06ed6a34cff0fa27dc6c87891a07e50
arcpp.zju <arcpp.zju@029d7cd8-c984-11de-a7ce-03bda429b547>
2010-03-30 16:22:53 +0000

    various updates

---
+++ startup/autoload.lua
@@ -0,0 +1,7 @@
+local current_version = "0.2.3"
+
+ime.register_command(0, 0 , "查看输入法版本", "ime.notify('输入法版本', '正在使用的版本：'..ime.VERSION..'\n最新的版本：'.."..current_version.."', 'info')")
+
+
+-- ibus-sogoupycc-autoload-end --
+
